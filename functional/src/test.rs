92df276e4f6dad896d531adf5e49c9352308c4db
PolyWolf <31190026+p0lyw0lf@users.noreply.github.com>
2024-11-17 00:17:27 +0000

    wip: get base case generation working

--- functional/src/test.rs
+++ functional/src/test.rs
@@ -2,6 +2,8 @@ use functional_macros::ast;
 
 #[ast]
 mod ast {
+    use crate::Functor;
+
     #[derive(Debug, PartialEq)]
     pub struct Function {
         pub name: String,
@@ -24,6 +26,8 @@ mod ast {
 
 use ast::*;
 
+use crate::Functor;
+
 #[test]
 fn base_traits() {
     let x = Function {
@@ -37,4 +41,32 @@ fn base_traits() {
             body: [Statement::Return(Exp::Lit(42))].into(),
         }
     );
+}
+
+#[test]
+fn uniplate() {
+    let x = Exp::Add {
+        lhs: Box::new(Exp::Lit(1)),
+        rhs: Box::new(Exp::Lit(2)),
+    };
+
+    // Unfortunately, we don't go inside nodes just yet 😔
+    let x_expected = Exp::Add {
+        lhs: Box::new(Exp::Lit(1)),
+        rhs: Box::new(Exp::Lit(2)),
+    };
+
+    let mut transform = |e: Exp| match e {
+        Exp::Lit(i) => Exp::Lit(i + 2),
+        otherwise => otherwise,
+    };
+
+    let x_actual = x.fmap(&mut transform);
+
+    assert_eq!(x_expected, x_actual);
+
+    let y = Exp::Lit(1);
+    let y_expected = Exp::Lit(3);
+    let y_actual = y.fmap(&mut transform);
+    assert_eq!(y_expected, y_actual);
 }

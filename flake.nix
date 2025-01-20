b46553cd6bdece9ac79c1e13f3f8ab4893815787
PolyWolf <31190026+p0lyw0lf@users.noreply.github.com>
2025-01-20 13:14:45 -0500

    flake update

--- flake.nix
+++ flake.nix
@@ -5,41 +5,48 @@
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
-      inputs.flake-utils.follows = "flake-utils";
     };
   };
 
-  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
-    flake-utils.lib.eachDefaultSystem (system:
-    let
-      overlays = [ (import rust-overlay) ];
-      pkgs = import nixpkgs {
-        inherit system overlays;
-      };
-
-      rust-bin = pkgs.rust-bin.stable.latest.default.override {
-        extensions = [
-          "rust-analyzer"
-          "rust-src"
-        ];
-      };
-
-      buildInputs = [
-        rust-bin
-      ];
-      nativeBuildInputs = with pkgs; [
-        # For driver script
-        bash
-        gcc
-        gdb
-        jq
-        # For book tests
-        python3
-      ];
-    in
+  outputs =
     {
-      devShells.default = pkgs.mkShell {
-        inherit buildInputs nativeBuildInputs;
-      };
-    });
+      self,
+      nixpkgs,
+      flake-utils,
+      rust-overlay,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+
+        rust-bin = pkgs.rust-bin.stable.latest.default.override {
+          extensions = [
+            "rust-analyzer"
+            "rust-src"
+          ];
+        };
+
+        buildInputs = [
+          rust-bin
+        ];
+        nativeBuildInputs = with pkgs; [
+          # For driver script
+          bash
+          gcc
+          gdb
+          jq
+          # For book tests
+          python3
+        ];
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          inherit buildInputs nativeBuildInputs;
+        };
+      }
+    );
 }
